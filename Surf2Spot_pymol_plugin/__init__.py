46b62ce3bcdb81a8583d686dcf31fe0968525933
AnwZhao <anwzhao@gmail.com>
2025-07-29 16:33:46 +0800

    first

---
+++ Surf2Spot_pymol_plugin/__init__.py
@@ -0,0 +1,13 @@
+# Pablo Gainza Cirauqui 2016 LPDI IBI STI EPFL
+# This pymol plugin for Masif just enables the load ply functions. 
+
+from pymol import cmd
+from .loadhsPLY import *
+from .loadhsDOTS import *
+import sys
+
+def __init_plugin__(app):
+    cmd.extend('loadhsply', load_hs_ply)
+    cmd.extend('loadhsdots', load_hs_dots)
+    cmd.extend('loadhsgiface', load_hs_giface)
+
